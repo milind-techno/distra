586689e62b5ddfbc8be007ab632224c9d25402a1
Ben Poulson <ben.poulson@jagex.com>
2020-12-02 17:31:14 +0000

    Cleanup

--- docker/elk-apm/create-selfsigned-ssl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-mkdir -p tmp/
-openssl genrsa -out tmp/selfsigned.key 2048
-openssl req -new -key tmp/selfsigned.key -out tmp/selfsigned.csr
-openssl x509 -req -days 365 -in tmp/selfsigned.csr -signkey tmp/selfsigned.key -out tmp/selfsigned.crt
-cat tmp/selfsigned.key tmp/selfsigned.crt > ssl.pem
-rm -rf tmp/
