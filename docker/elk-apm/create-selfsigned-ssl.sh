c73774178764f8434ac25bc13eecc50c08467062
Lee Kerby <lee.kerby@jagex.com>
2020-12-02 16:22:15 +0000

    Removed https. Added APM configuration

--- docker/elk-apm/create-selfsigned-ssl.sh
+++ docker/elk-apm/create-selfsigned-ssl.sh
@@ -4,4 +4,4 @@ openssl genrsa -out tmp/selfsigned.key 2048
 openssl req -new -key tmp/selfsigned.key -out tmp/selfsigned.csr
 openssl x509 -req -days 365 -in tmp/selfsigned.csr -signkey tmp/selfsigned.key -out tmp/selfsigned.crt
 cat tmp/selfsigned.key tmp/selfsigned.crt > ssl.pem
-rm -rf tmp/
+#rm -rf tmp/
